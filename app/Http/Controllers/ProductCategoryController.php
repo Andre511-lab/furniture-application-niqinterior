6f81c1a31bfbf3eab92abc4413130d7d7c2f45ab
Muhammad Andre <andrekrena@gmail.com>
2022-04-23 23:58:58 +0700

    adding product category controller, modified route for category and  make CMS page for category

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use App\Http\Requests\ProductCategoryRequest;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(request()->ajax())
+        {
+            $query = ProductCategory::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function($item){
+                    return '
+                        <form class="inline-block" action="'. route('dashboard.product.destroy', $item->id) .'" method="POST">
+                            <button class="border border-red-500 bg-red-500 text-white rounded-md px-2 py-1 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline">
+                                Delete
+                            </button>
+                        '. method_field('delete') . csrf_field() .'
+                        </form>
+                    ';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+        return view('pages.dashboard.category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.dashboard.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductCategoryRequest $request)
+    {
+        $data = $request->all();
+        $data['category_slug'] = Str::slug($request->name);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductCategory $category)
+    {
+        $category ->delete();
+
+        return redirect()->route('dashboard.category.index');
+    }
+}
